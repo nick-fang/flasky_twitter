44685825ef83260f1cc6e02b1472afd0608edda7
FangYiling <36231529+nick-fang@users.noreply.github.com>
2019-05-14 14:56:26 +0800

    Update config.py

--- config.py
+++ config.py
@@ -8,10 +8,8 @@ class Config:
     MAIL_PORT = int(os.environ.get('MAIL_PORT', '587'))
     MAIL_USE_SSL = False
     MAIL_USE_TLS = True
-    #MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    #MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    MAIL_USERNAME = 'caihuyougui@qq.com'
-    MAIL_PASSWORD = 'bpnqywrigcvcbccg'
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME') #你的邮箱账号
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') #你的邮箱客户端授权码
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
     FLASKY_MAIL_SENDER = 'Flasky Admin <caihuyougui@qq.com>'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN', 'caihuyougui@qq.com')
